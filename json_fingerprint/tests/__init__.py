d52a408bf7b49bb4c74930d2cc9d12035833625a
Ville Lehtinen <ville.lehtinen@cobaltine.fi>
2021-01-05 02:07:49 +0200

    Add simple test suite runner #minor
    Added an executable test suite runner, easily runnable in any installed
    environment, and from an idiomatic execution path.
    
    Other changes:
     - Updated README.md with a Table of Contents (uses Atom's `md-toc`
     plugin)

--- json_fingerprint/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from .test_decode_fingerprint import TestDecodeFingerprint
-from .test_fingerprint_match import TestFingerprintMatch
-from .test_jfpv1 import TestJfpv1
-from .test_json_fingerprint import TestJsonFingerprint
-from .test_validators import TestValidators
