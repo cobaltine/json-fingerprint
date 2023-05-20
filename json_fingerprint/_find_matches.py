d44bc0a9c5024eb95cd6b4c32dc1e7b0ebf73ae0
Ville Lehtinen <ville.lehtinen@cobaltine.fi>
2023-05-20 19:56:19 +0300

    Add proper Google-style docstrings to public API

--- json_fingerprint/_find_matches.py
+++ json_fingerprint/_find_matches.py
@@ -16,6 +16,7 @@ def _get_target_hashes(fingerprints: List[str]) -> List[Dict]:
 
 
 def _create_input_fingerprints(input: str, target_hashes: List[Dict]) -> List[str]:
+    """Create all necessary JSON fingerprint variations of the JSON data input."""
     input_fingerprints = []
     for element in target_hashes:
         fingerprint = create(input=input, hash_function=element["hash_function"], version=element["version"])
@@ -24,10 +25,27 @@ def _create_input_fingerprints(input: str, target_hashes: List[Dict]) -> List[st
 
 
 def find_matches(input: str, fingerprints: List[str], deduplicate: bool = False) -> List[str]:
-    """Match raw json string input to a list of fingerprints
+    """Match raw json string input to a list of fingerprints.
 
-    Decodes the target fingerprints and creates a fingerprint from the input with identical parameters.
-    Creates a fingerprint from the input of each different JSON fingerprint type present in the fingerprint list.
+    The fingerprint matching is executed as follows:
+        1. Optional: deduplicate the target fingerprint list
+        2. Decode the target fingerprint list to find all different JSON fingerprint variations (hash function, version)
+        3. Create JSON fingerprints from the input with identical parameters (e.g., all hash function variations)
+        4. Compare the input JSON fingerprint variations to the target list and find all matches
+
+    If there is e.g. "sha256" and "sha512" JSON fingerprint variations in the target list of fingerprints,
+    then a JSON fingerprint is created from the input JSON data with both the "sha256" and the "sha512" hash functions.
+
+    Args:
+        input (str):
+            JSON input in string format.
+        fingerprints (list of strings):
+            A list of JSON fingerprints in string format.
+        deduplicate (bool):
+            If True, then deduplicate the fingerprint list before processing matches. False by default.
+
+    Returns:
+        list: A list of JSON fingerprint matches in string format.
     """
     if deduplicate:
         fingerprints = list(set(fingerprints))
